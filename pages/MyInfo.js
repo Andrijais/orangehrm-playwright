b5edbe8587ceff4c8b686c248a1d151c223bfc14
Andri <jaiswalandri@gmail.com>
2026-02-16 13:11:02 +0530

    4th

--- pages/MyInfo.js
+++ pages/MyInfo.js
@@ -1,27 +1,4 @@
-// export class MyInfo{
-//     constructor(page){
-//         this.page = page;
-//         this.MyInfoHeading = page.getByRole('heading',{name: 'PIM'});
-//         this.FirstNameInp = page.getByRole('input[name="firstName"]');
-//         this.MiddleNameInp = page.getByRole('placeholder',{name:'middlename'});
-//         this.LastNameInp = page.getByRole('placeholder',{name:'lastName'});
-//         this.SaveButton1 = page.getByRole('button',{type:'submit'});
 
-
-//     }
-
-//     async updatemyinfo(FirstNameInp,MiddleNameInp,LastNameInp){
-//         await this.FirstNameInp.fill(FirstNameInp);
-//         await this.middleNameInput.fill(MiddleNameInp);
-//         await this.lastNameInput.fill(LastNameInp);
-
-//     }
-
-//     async save() {
-//         await this.SaveButton1.click();
-//     }
-
-// }
 
 export class MyInfo {
     constructor(page) {
